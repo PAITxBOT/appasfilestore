f292841b9c1d851f7778b9722b891dde4a0aa635
PAITxBOT <143090682+PAITxBOT@users.noreply.github.com>
2024-03-31 07:23:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6480231470:AAHXckCiRTvYjLLgq4SQOrylCj4WtSKj9kg")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7125109254:AAHrGvVUARNWJtnmK_uJpo1xHFwHmesQbME")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "10261086"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "10261086"))
 API_HASH = os.environ.get("API_HASH", "9195dc0591fbdb22b5711bcd1f437dab")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001664717775"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002052333240"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6628688625"))
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://appasfilestore:appasfiles
 DB_NAME = os.environ.get("DATABASE_NAME", "appasfilestore")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001765815416"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001993099652"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6628688625 1426588906").split()):
+    for x in (os.environ.get("ADMINS", "6628688625 6599198780").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
